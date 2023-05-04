ca4d372fa55c7ee9e18b2411dd8c2b0ec053e0db
Shane Smith <110691416+ssmith34@users.noreply.github.com>
2023-05-04 16:02:38 -0400

    Finished challenge

--- src/ElectricCar.java
+++ src/ElectricCar.java
@@ -6,4 +6,20 @@ public class ElectricCar extends Car {
     public ElectricCar(String description) {
         super(description);
     }
+
+    public ElectricCar(String description, double avgKmPerLitre, int cylinders) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerLitre;
+        this.batterySize = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage under the average: %.2f %n", avgKmPerCharge);
+    }
 }
--- src/HybridCar.java
+++ src/HybridCar.java
@@ -1,10 +1,28 @@
 public class HybridCar extends Car {
 
     private double avgKmPerLitre;
-    private int batterySize;
     private int cylinders;
+    private int batterySize;
 
     public HybridCar(String description) {
         super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerLitre, int cylinders, int batterySize) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+        this.batterySize = batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up.%n", cylinders);
+        System.out.printf("Hybrid -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage below average: %.2f %n", avgKmPerLitre);
     }
 }
